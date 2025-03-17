c61b69d1047ac1a553fcbffdc7c24d5ce1c5be42
shijinpjlab <shijinpjlab@163.com>
2025-03-17 11:47:42 +0800

    feat: add detect politics scene. (#46)
    * feat: add compare scene.
    
    * feat: add compare scene test case.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: add PromptTextQualityV4.
    
    * feat: add detect politics scene.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat:
    add prompt prohibitions.
    extend detect_politics to detect_security, which including politics and prohibitions.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ examples/security/text_security.py
@@ -0,0 +1,28 @@
+from dingo.exec import Executor
+from dingo.io import InputArgs
+
+input_data = {
+    "input_path": "../../test/data/test_local_jsonl.jsonl",
+    "save_data": True,
+    "save_correct": True,
+    "dataset": "local",
+    "data_format": "jsonl",
+    "column_content": "content",
+    "custom_config":
+        {
+            "prompt_list": ["PromptPolitics"],
+            "llm_config":
+                {
+                    "detect_security":
+                        {
+                            "key": "",
+                            "api_url": "",
+                        }
+                }
+        },
+    "log_level": "INFO"
+}
+input_args = InputArgs(**input_data)
+executor = Executor.exec_map["local"](input_args)
+result = executor.execute()
+print(result)
