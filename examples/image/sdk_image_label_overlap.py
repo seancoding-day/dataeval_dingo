e984a99bd63ba09e3cb218c8c457298135303994
shijinpjlab <shijinpjlab@163.com>
2025-10-09 14:32:51 +0800

    feat: example

---
+++ examples/image/sdk_image_label_overlap.py
@@ -0,0 +1,32 @@
+from dingo.config import InputArgs
+from dingo.exec import Executor
+
+
+def image_text_similar():
+    input_data = {
+        "input_path": "../../test/data/img_label/test_img_label_overlap.jsonl",
+        "dataset": {
+            "source": "local",
+            "format": "image",
+            "field": {
+                "id": "id",
+                "content": "content",
+                "image": "img"
+            }
+        },
+        "executor": {
+            "rule_list": ["RuleImageLabelOverlap"],
+            "result_save": {
+                "bad": True,
+                "good": True
+            }
+        }
+    }
+    input_args = InputArgs(**input_data)
+    executor = Executor.exec_map["local"](input_args)
+    result = executor.execute()
+    print(result)
+
+
+if __name__ == '__main__':
+    image_text_similar()
