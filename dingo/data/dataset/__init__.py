eb4a8589360ddc2ad436cdd7c054e1204706ef4c
chupei <njuchupei@gmail.com>
2025-10-16 19:24:32 +0800

    feat: fix s3 source and add example

--- dingo/data/dataset/__init__.py
+++ dingo/data/dataset/__init__.py
@@ -4,6 +4,12 @@ from dingo.data.dataset.local import LocalDataset  # noqa E402.
 from dingo.utils import log
 
 try:
+    from dingo.data.dataset.s3 import S3Dataset  # noqa E402.
+except Exception as e:
+    log.warning("S3 Dataset not imported. Open debug log for more details.")
+    log.debug(str(e))
+
+try:
     from dingo.data.dataset.spark import SparkDataset  # noqa E402.
 except Exception as e:
     log.warning("Spark Dataset not imported. Open debug log for more details.")
