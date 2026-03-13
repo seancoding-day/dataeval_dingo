eae2c92eafd4ae562cc1a018b809517d104cf12a
sjshailab <shijinpjlab@163.com>
2026-03-13 14:47:17 +0800

    feat: version 2.1.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ web_static_files = get_data_files('web-static')
 
 setup(
     name="dingo-python",
-    version="2.1.0",
+    version="2.1.1",
     author="Dingo",
     description="A Comprehensive AI Data Quality Evaluation Tool for Large Models",
     long_description=long_description,
