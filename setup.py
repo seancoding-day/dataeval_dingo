cc10901f28813dc5f0c7e05043aebefc4109650a
Pei Chu <chupei@pjlab.org.cn>
2026-05-29 18:27:54 +0800

    Release v2.4.0 (#413)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras_require = {
 
 setup(
     name="dingo-python",
-    version="2.3.0",
+    version="2.4.0",
     author="Dingo",
     description="A Comprehensive AI Data Quality Evaluation Tool for Large Models",
     long_description=long_description,
